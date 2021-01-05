78641a6934a23c1be1b9c18c427d4ac4e21be67a
helge <helgerannem@gmail.com>
2021-01-05 12:10:30 +0100

    somethign to start with

---
+++ app/src/main/java/dev/kotlin/unwind/MainActivity.kt
@@ -0,0 +1,63 @@
+package dev.kotlin.unwind
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        btnRegister.setOnClickListener {
+            if (editEmail.text.trim().toString().isNotEmpty() || editPassword.text.trim()
+                    .isNotEmpty()
+            ) {
+                createUser(editEmail.text.trim().toString(), editPassword.text.trim().toString())
+
+            } else {
+
+                Toast.makeText(this, "Input required", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+        fun createUser(email: String, password: String) {
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+
+                    if (task.isSuccessful) {
+                        Log.e("Task Message", "Succcessfull")
+
+                        var intent = Intent(this, DashboardActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Log.e("Task Message", "Failed"+task.exception)
+                    }
+
+                }
+
+        }
+
+    override fun onStart() {
+        super.onStart()
+        val user = auth.currentUser
+
+        if (user != null) {
+            var intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+
+    }
