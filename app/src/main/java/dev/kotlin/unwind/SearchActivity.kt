e4c6cf2aadc170a9e5c8cc4395748294ace7cd26
Stryningen <fredrsan@gmail.com>
2021-01-20 09:05:53 +0100

    base functionality of content search for tvShows done

--- app/src/main/java/dev/kotlin/unwind/SearchActivity.kt
+++ app/src/main/java/dev/kotlin/unwind/SearchActivity.kt
@@ -2,15 +2,19 @@ package dev.kotlin.unwind
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
+import dev.kotlin.unwind.models.Content
 import dev.kotlin.unwind.models.TvShow
+import org.json.JSONException
 import org.json.JSONObject
 
 class SearchActivity : AppCompatActivity() {
@@ -18,7 +22,11 @@ class SearchActivity : AppCompatActivity() {
     private lateinit var etSearchField: EditText
     private lateinit var btnSearchContent: Button
     private lateinit var rvTvContent: RecyclerView
-    //delete testing textview!!!
+
+    private lateinit var adapter: SearchActivityAdapter
+    private lateinit var content: MutableList<Content?>
+
+    //delete testing textView!!!
     private lateinit var tvTestingPurpose: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,22 +39,40 @@ class SearchActivity : AppCompatActivity() {
         //delete testing purpose!!
         tvTestingPurpose = findViewById(R.id.tvTestingPurpose)
 
-        btnSearchContent.setOnClickListener{
+        btnSearchContent.setOnClickListener {
             searchContent()
         }
+
+        content = mutableListOf()
+        adapter = SearchActivityAdapter(this, content)
+
+        rvTvContent.adapter = adapter
+        rvTvContent.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true)
+
     }
 
     private fun searchContent() {
         //given input in the search field should handle search for content
         val queue = Volley.newRequestQueue(this)
-        val url = "https://api.tvmaze.com/search/shows?q=girls"
+        val url = "https://api.tvmaze.com/search/shows?q=${etSearchField.text}"
+        var show: TvShow?
 
         val jsonRequest = JsonArrayRequest(Request.Method.GET, url, null,
-        Response.Listener{
-            val i = 0
-            val show = parseJsonObjectToTvShow(it.getJSONObject(i))
-        },
-            Response.ErrorListener {error ->
+            Response.Listener {
+                content.clear()
+                if (it.length() != 0){
+                    for (i in 0 until it.length()){
+                        show = parseJsonObjectToTvShow(it.getJSONObject(i))
+                        tvTestingPurpose.text = ""
+                        content.add(show)
+                    }
+                } else {
+                    tvTestingPurpose.text = "No result"
+                    content.clear()
+                }
+                adapter.notifyDataSetChanged()
+            },
+            Response.ErrorListener { error ->
                 tvTestingPurpose.text = error.message
             }
         )
@@ -55,6 +81,56 @@ class SearchActivity : AppCompatActivity() {
 
     private fun parseJsonObjectToTvShow(jsonObject: JSONObject?): TvShow? {
         val show = jsonObject?.getJSONObject("show")
-        return show?.getInt("id")?.let { TvShow(it, show.getString("name")) }
+        val contentId: Int
+        val title: String
+        val genres = mutableListOf<String>()
+        var smallImage: String
+        var largeImage: String
+
+
+        try {
+            contentId = show?.getInt("id")!!
+        } catch (e: JSONException) {
+            return null
+        }
+
+        title = try {
+            show?.getString("name")
+        } catch (e: JSONException) {
+            "no_name"
+        }
+
+        smallImage = try {
+            show?.getJSONObject("image")?.getString("medium").toString()
+        } catch (e: JSONException) {
+            "no_image"
+        }
+
+        if (!smallImage.equals("no_image")){
+            smallImage = "https://" + smallImage.substring(6)
+        }
+
+        largeImage = try {
+            show?.getJSONObject("image")?.getString("original").toString()
+        } catch (e: JSONException) {
+            "no_image"
+        }
+
+        if (!largeImage.equals("no_image")){
+            largeImage = "https://" + largeImage.substring(6)
+        }
+
+        try {
+            val jsonArrayGenres = show?.getJSONArray("genres")
+            if (jsonArrayGenres != null) {
+                for (i in 0 until jsonArrayGenres.length()) {
+                    genres.add(jsonArrayGenres.getString(i))
+                }
+            }
+
+        } catch (e: JSONException) {
+            genres.add("undefined")
+        }
+        return TvShow(contentId, title, smallImage, largeImage, genres)
     }
 }
--- app/src/main/java/dev/kotlin/unwind/models/TvShow.kt
+++ app/src/main/java/dev/kotlin/unwind/models/TvShow.kt
@@ -1,12 +1,17 @@
 package dev.kotlin.unwind.models
 
+import android.net.Uri
+
 class TvShow(
     private val contentId: Int,
-    private val title: String
+    private val title: String,
+    private val imageUrlSmall: String,
+    private val imageUrlLarge: String,
+    private val genres: List<String>
 ) : Content
 {
 
-    private var averageRating: Double = 0.0
+    private var averageRating: Double = -1.0
 
     override val contentType = ContentType.TV_SHOW
 
@@ -17,5 +22,29 @@ class TvShow(
     override fun getContentId(): Int {
         return contentId
     }
+
+    override fun getCoverImageUrl(): String {
+        return imageUrlSmall
+    }
+
+    fun getImageUrlSmall(): String {
+        return imageUrlSmall
+    }
+    fun getImageUrlLarge(): String {
+        return imageUrlLarge
+    }
+
+    fun getGenres(): List<String> {
+        return genres
+    }
+
+    fun setAverageRating(newRating: Double) {
+        averageRating = newRating
+    }
+
+    fun getAverageRating(): Double {
+        return averageRating
+    }
+
 
 }
