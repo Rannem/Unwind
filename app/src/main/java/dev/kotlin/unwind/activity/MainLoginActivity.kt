b9eead424e46cb4f2e051828b87d27a0d5163525
Sindre <sindreset@gmail.com>
2021-01-26 13:00:10 +0100

    general styling ( login view, search view, content card, logo, content card )

--- app/src/main/java/dev/kotlin/unwind/activity/MainLoginActivity.kt
+++ app/src/main/java/dev/kotlin/unwind/activity/MainLoginActivity.kt
@@ -2,9 +2,12 @@ package dev.kotlin.unwind.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import dev.kotlin.unwind.R
 
 class MainLoginActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_login)
